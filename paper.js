d63b0f74923bc4a92cb08a68b5aa14684403fae6
dina747 <69771997+dina747@users.noreply.github.com>
2021-01-25 20:57:34 -0500

    Update paper.js

--- paper.js
+++ paper.js
@@ -14,21 +14,22 @@ class paper
 		this.y=y;
 		this.r=r
 		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		this.image=loadImage("dona.png")
 		World.add(world, this.body);
 
 	}
 	display()
 	{
 			
-		loadImage("dona.png");
+		
 		var paperpos=this.body.position;		
 
 			push()
 			translate(paperpos.x, paperpos.y);
-			rectMode(CENTER)
+		    imageMode(CENTER)
 			strokeWeight(3);
 			fill(255,0,255)
-			ellipse(0,0,this.r, this.r);
+			image(this.image,0,0,this.r, this.r);
 			pop()
 			
 	}
